2c06e86d87457ac2c29058b9f1da0de3f8edf63b
kienta1999 <talekien1710@gmail.com>
2022-02-07 19:01:31 -0600

    Xcode 12.1 project created

---
+++ BarberApp/BarberApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarberApp
+//
+//  Created by admin on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
