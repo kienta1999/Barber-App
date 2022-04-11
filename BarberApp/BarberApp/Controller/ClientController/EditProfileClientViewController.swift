d4a493b6bc05c54b25bfd8f445ca92a0f2305708
kienta1999 <talekien1710@gmail.com>
2022-04-11 17:58:57 -0500

    display ui for edit profile

--- BarberApp/BarberApp/Controller/ClientController/EditProfileClientViewController.swift
+++ BarberApp/BarberApp/Controller/ClientController/EditProfileClientViewController.swift
@@ -9,10 +9,55 @@ import UIKit
 
 class EditProfileClientViewController: HomepageViewController {
 
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    
+    @IBOutlet weak var bioTextView: UITextView!
+    
+    @IBOutlet weak var maleButton: UIButton!
+    @IBOutlet weak var femaleButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
+    
+    var gender: String =  "Male"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        maleButton.isSelected = true
+        if let currGender = user?.gender{
+            gender = currGender
+            if currGender == "Female" {
+                maleButton.isSelected = false
+                femaleButton.isSelected = true
+            }
+            else if currGender == "Other" {
+                maleButton.isSelected = false
+                otherButton.isSelected = true
+            }
+        }
+        if let age = user?.age {
+            ageTextField.text = "\(age)"
+        }
+        if let bio = user?.bio {
+            bioTextView.text = bio
+        }
+        
     }
+    
+    @IBAction func genderButtonClicked(_ sender: UIButton) {
+        maleButton.isSelected = false
+        femaleButton.isSelected = false
+        otherButton.isSelected = false
+        sender.isSelected = true
+        gender = sender.currentTitle!
+    }
+    
+    
+    @IBAction func saveProfile(_ sender: Any) {
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
 
 }
