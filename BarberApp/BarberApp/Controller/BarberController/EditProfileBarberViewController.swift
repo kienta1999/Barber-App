b2a149e6e3ceda7ac778c60ed3b0e7bfe7fe5378
kienta1999 <talekien1710@gmail.com>
2022-04-14 00:09:12 -0500

    Address search

--- BarberApp/BarberApp/Controller/BarberController/EditProfileBarberViewController.swift
+++ BarberApp/BarberApp/Controller/BarberController/EditProfileBarberViewController.swift
@@ -7,16 +7,43 @@
 
 import UIKit
 import FirebaseStorage
+import MapKit
 
-class EditProfileBarberViewController: EditProfileViewController {
+class EditProfileBarberViewController: EditProfileViewController, MKLocalSearchCompleterDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var addressSearchBar: UISearchBar!
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        searchCompleter.delegate = self
+        addressSearchBar.delegate = self
+
         if let phone = user?.phoneNumber{
             phoneTextField.text = String(phone)
         }
     }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+            searchResults = completer.results
+            print(searchResults)
+        }
+
+    private func completer(completer: MKLocalSearchCompleter, didFailWithError error: NSError) {
+            // handle error
+        }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
+    
+    @IBAction func seeAddress(_ sender: UIButton) {
+        print(searchResults)
+    }
+    
     
     
 
