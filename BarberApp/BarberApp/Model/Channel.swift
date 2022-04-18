70ca290f6d1eeeb7e7fa790eeaa7d563b4b43013
jwchoi1128 <jiwonchoi@wustl.edu>
2022-04-18 18:40:12 -0500

    stash

---
+++ BarberApp/BarberApp/Model/Channel.swift
@@ -0,0 +1,29 @@
+//
+//  Channel.swift
+//  BarberApp
+//
+//  Created by 최지원 on 4/18/22.
+//
+
+import Foundation
+
+struct Channel {
+    var id: String?
+    let name: String
+    
+    init(id: String? = nil, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+extension Channel: Comparable {
+    static func == (lhs: Channel, rhs: Channel) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func < (lhs: Channel, rhs: Channel) -> Bool {
+        return lhs.name < rhs.name
+    }
+}
+
